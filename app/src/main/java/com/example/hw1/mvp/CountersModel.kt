ce7cf95466c0a6c1fa673ac4bff9f21836e5a2b2
gansergey <gaaan@outlook.com>
2021-12-04 15:08:39 +0300

    HW1

---
+++ app/src/main/java/com/example/hw1/mvp/CountersModel.kt
@@ -0,0 +1,19 @@
+package com.example.hw1.mvp
+
+class CountersModel {
+
+    private val counters = mutableListOf(0, 0, 0)
+
+    private fun getCurrent(index: Int): Int {
+        return counters[index]
+    }
+
+    fun next(index: Int): Int {
+        counters[index]++
+        return getCurrent(index)
+    }
+
+    fun set(index: Int, value: Int){
+        counters[index] = value
+    }
+}
---
+++ app/src/main/java/com/example/hw1/mvp/MainView.kt
@@ -0,0 +1,7 @@
+package com.example.hw1.mvp
+
+interface MainView {
+    fun setButton1Text(text: String)
+    fun setButton2Text(text: String)
+    fun setButton3Text(text: String)
+}
